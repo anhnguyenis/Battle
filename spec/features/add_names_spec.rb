2e512f4108926da54507b7c72799d98b8eba9113
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-19 17:28:55 +0000

    add web_helper.rb and refactor tests

--- spec/features/add_names_spec.rb
+++ spec/features/add_names_spec.rb
@@ -1,9 +1,7 @@
 feature 'Adds player names ' do
   scenario 'Can fill in player names and display them onscreen' do
     visit('/')
-      fill_in 'name1', with: 'Bob'
-      fill_in 'name2', with: 'Sheila'
-      click_button 'Submit'
+      sign_in_and_play
       expect(page).to have_content 'Bob vs. Sheila'
   end
 end
--- spec/features/points_spec.rb
+++ spec/features/points_spec.rb
@@ -1,9 +1,7 @@
 feature 'See Player 2s Hit Points' do
   scenario 'Can display player 2s points' do
     visit('/')
-    fill_in 'name1', with: 'Bob'
-    fill_in 'name2', with: 'Sheila'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content'Sheila has 12 points'
   end
 end
