214a85af0999dcf56bc782c730edbb187fbaf7c7
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-19 15:58:33 +0000

    refactor test to change button for player 2 points, to a link for player 2 points

--- spec/features/points_spec.rb
+++ spec/features/points_spec.rb
@@ -1,7 +1,7 @@
 feature 'See Player 2s Hit Points' do
   scenario 'Can display player 2s points' do
     visit('/play')
-      click_button 'Show points'
+      find('#name2').click
       expect(page).to have_content 'Player 2 has 12 points'
   end
 end
