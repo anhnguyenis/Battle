fe18521fee5b004e113de7d6672fb99248d5fa00
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-19 16:54:11 +0000

    update feature test to show points on play page following redirect

--- spec/features/points_spec.rb
+++ spec/features/points_spec.rb
@@ -1,7 +1,9 @@
 feature 'See Player 2s Hit Points' do
   scenario 'Can display player 2s points' do
-    visit('/play')
-      find('#name2').click
-      expect(page).to have_content 'Player 2 has 12 points'
+    visit('/')
+    fill_in 'name1', with: 'Bob'
+    fill_in 'name2', with: 'Sheila'
+    click_button 'Submit'
+    expect(page).to have_content'Sheila has 12 points'
   end
 end
