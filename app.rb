6e1856dd1387883cd99f9ce318a15aa53324d63b
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-21 10:30:58 +0000

    extract Player class and pass session to Player class

--- app.rb
+++ app.rb
@@ -13,22 +13,22 @@ get '/' do
 end
 
 post '/names' do
-  session[:name1] = params[:name1]
-  session[:name2] = params[:name2]
+  $player1 = params[:name1]
+  $player2 = params[:name2]
     # @name1 = params[:name1]
     # @name2 = params[:name2]
   redirect('/play')
 end
 
 get '/play' do
-  @name1 = session[:name1]
-  @name2 = session[:name2]
+  @name1 = $player1
+  @name2 = $player2
   erb :play
 end
 
 get '/attack' do
-  @name1 = session[:name1]
-  @name2 = session[:name2]
+  @name1 = $player1
+  @name2 = $player2
   erb :attack
 end
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,6 +1,8 @@
+require 'player'
+
 describe Player do
   it 'returns player name' do
-    player = Player.new
-    expect(player.name).to eq('Bob')    
+    player = Player.new('Bob')
+    expect(player.name).to eq 'Bob'
   end
 end
