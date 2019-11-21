5f9f113aff0ffb0beeaf7c811a199854e73e36f6
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-21 11:18:49 +0000

    add feature test to reduce player 2 points by 10

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,13 @@
 feature 'Attack Player 2 and display confirmation' do
   scenario 'Can hit player 2 and display onscreen' do
     sign_in_and_play
-    click_link 'Hit Player 2'
+    click_on 'Hit Sheila'
     expect(page).to have_content 'You have gently nudged Sheila!'
+  end
+
+  scenario 'reduce player 2s points by 10' do
+    sign_in_and_play
+    click_on 'Hit Sheila'
+    expect(page).to have_content 'Sheila now has 2 points.'
   end
 end
--- spec/features/points_spec.rb
+++ spec/features/points_spec.rb
@@ -1,6 +1,6 @@
 feature 'See Player 2s Hit Points' do
   scenario 'Can display player 2s points' do
     sign_in_and_play
-    expect(page).to have_content'Sheila has 12 points'
+    expect(page).to have_content'Sheila currently has 12 hit points'
   end
 end
