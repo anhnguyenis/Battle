006565ee7007e1150c4e5f9a45e24741e13a0bdc
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-21 11:56:33 +0000

    refactor player_spec tests to declare subject

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,17 +1,17 @@
 require 'player'
 
 describe Player do
-
+  subject(:player1) {Player.new('Bob')}
+  subject(:player2) {Player.new('Sheila')}
 describe '#name' do
   it 'returns player name' do
-    player1 = Player.new('Bob')
+    # player = Player.new('Bob')
     expect(player1.name).to eq 'Bob'
   end
 end
 
 describe '#hit_points' do
   it 'returns hit points' do
-    player1 = Player.new('Bob')
     expect(player1.hit_points).to eq described_class::DEFAULT_HIT_POINTS
   end
 end
