70bb85eb4f43f48fe7adaf330679ed1fd171840d
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-21 11:55:18 +0000

    add test for accessing player instance hit_points

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -8,6 +8,7 @@ feature 'Attack Player 2 and display confirmation' do
   scenario 'reduce player 2s points by 10' do
     sign_in_and_play
     click_on 'Hit Sheila'
+    expect(page).not_to have_content 'Sheila now has 12 points'
     expect(page).to have_content 'Sheila now has 2 points.'
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,19 @@
 require 'player'
 
 describe Player do
+
+describe '#name' do
   it 'returns player name' do
-    player = Player.new('Bob')
-    expect(player.name).to eq 'Bob'
+    player1 = Player.new('Bob')
+    expect(player1.name).to eq 'Bob'
   end
+end
+
+describe '#hit_points' do
+  it 'returns hit points' do
+    player1 = Player.new('Bob')
+    expect(player1.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+  end
+end
+
 end
